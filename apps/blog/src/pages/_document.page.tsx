d95f52ca36198a704dd74d2974b00d2a7a23aa0e
Caleb Forestal <133164966+Calebslc@users.noreply.github.com>
2023-09-20 18:39:49 -0800

    a

--- apps/blog/src/pages/_document.page.tsx
+++ apps/blog/src/pages/_document.page.tsx
@@ -43,22 +43,6 @@ export default class BlogDocument extends Document {
             </>
           )}
 
-          {/* for hotjar */}
-          {isValid(blogHotjarID) && (
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `(function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:${blogHotjarID},hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-              }}
-            ></script>
-          )}
-
           <GlobalStyle />
         </Head>
 
